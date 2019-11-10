866a34c4b8e47fb02a90286eec5d41607373d79c
Anastasiya Hahanova <anastasiya.hahanova@mail.ru>
2019-11-10 18:51:40 +0300

    footer

---
+++ src/js/hn.js
@@ -0,0 +1,35 @@
+// Google Web Font API
+WebFontConfig = {
+    google: { families: [ 'Open+Sans::latin' ] }
+};
+(function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+        '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+})();
+
+// Custom jQuery
+var parent = $("#mobile-nav").children('ul').children('li');
+
+function postloadFunc() {
+    $(parent).find('ul').addClass('dropdown');
+    $('.dropdown').parent('li').addClass('parent');
+}
+
+postloadFunc();
+
+$(parent).on('mouseenter', function (event) {
+    event.preventDefault();
+    $(this).removeClass('closed');
+    $(this).children('a').addClass('hovered');
+});
+
+$(parent).on('mouseleave', function(event) {
+    event.preventDefault();
+    $(this).addClass('closed');
+    $(parent).children('a').removeClass('hovered');
+});
